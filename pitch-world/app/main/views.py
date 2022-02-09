b0e3f49046577ed7d402b4213356b56ad20c89f5
segem001 <kipdom001@gmail.com>
2022-02-09 13:04:11 +0300

    define the new pitch function

--- pitch-world/app/main/views.py
+++ pitch-world/app/main/views.py
@@ -12,3 +12,16 @@ def index():
     event = Pitch.query.filter_by(category = 'Events').all()
     advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
     return render_template('index.html', job = job,event = event, pitches = pit
+
+@main.route('/create_new', methods = ['POST','GET'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user
+        new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
+        new_pitch_object.save_p()
+        return redirect(url_for('main.index'))
